e8775e70d6990888e8958787065190c7af4f3ce3
prakhar230620 <prakharbankhede15@gmail.com>
2026-04-29 23:19:56 +0530

    feat: implement full-stack authentication, note management, and admin dashboard features

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ const express = require("express")
 require("dotenv").config()
 const connectDB = require("./config/db")
 const cors = require("cors")
-const path = require("path")
 
 const app = express()
 
@@ -17,14 +16,6 @@ app.use(cors())
 app.use("/api/users", require("./routes/users"))
 app.use("/api/auth", require("./routes/auth"))
 app.use("/api/notes", require("./routes/notes"))
-
-// Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"))
-
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")))
-}
 
 const PORT = process.env.PORT || 50000
 
